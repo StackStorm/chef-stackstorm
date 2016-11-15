4c0d1512137782733da95a2f5250fb45afe08fa6
armab <dev@arma.biz>
2016-11-15 17:20:32 +0300

    Use 127.0.0.1 instead of localhost by default for consistency
    Convention we started to follow quite a while for all config settings where possible, see:
    https://github.com/StackStorm/st2/blob/master/conf/st2.package.conf

--- attributes/config.rb
+++ attributes/config.rb
@@ -2,7 +2,7 @@ include_attribute 'stackstorm::user'
 
 # Config defaults options
 default['stackstorm']['config'] = {
-  api_url: 'http://localhost:9101',
+  api_url: 'http://127.0.0.1:9101',
   api_host: '0.0.0.0',
   api_port: 9101,
   debug: false,
@@ -10,7 +10,7 @@ default['stackstorm']['config'] = {
   auth_port: 9100,
   auth_use_ssl: false,
   auth_enable: true,
-  rmq_host: 'localhost',
+  rmq_host: '127.0.0.1',
   rmq_vhost: nil,
   rmq_username: 'guest',
   rmq_password: 'guest',
@@ -18,13 +18,13 @@ default['stackstorm']['config'] = {
   allow_origin: '*',
   auth_standalone_file: '/etc/st2/htpasswd',
   syslog_enabled: false,
-  syslog_host: 'localhost',
+  syslog_host: '127.0.0.1',
   syslog_port: 514,
   syslog_facility: 'local7',
   syslog_protocol: 'udp',
   system_user: node['stackstorm']['user']['user'],
   ssh_key_file: "#{node['stackstorm']['user']['home']}/.ssh/id_rsa",
-  db_host: 'localhost',
+  db_host: '127.0.0.1',
   db_port: 27017,
   db_name: 'st2',
   db_username: nil,
