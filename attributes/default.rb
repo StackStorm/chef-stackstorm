f3016edb9c0676249933fce5251fb647cceb7006
armab <dev@arma.biz>
2016-11-22 19:52:39 +0200

    Move the list of st2 services to recipes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,1 @@
-# List of available `st2` services:
-# https://github.com/StackStorm/st2/blob/master/st2common/bin/st2ctl#L5
-default['stackstorm']['SERVICES'] = %w(st2actionrunner st2api st2stream st2auth st2garbagecollector st2notifier st2resultstracker st2rulesengine st2sensorcontainer).freeze
-
 default['stackstorm']['install_repo']['packages'] = %w(st2)
--- spec/recipes/_services_spec.rb
+++ spec/recipes/_services_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+# List of available `st2` services:
+# https://github.com/StackStorm/st2/blob/master/st2common/bin/st2ctl#L5
+ST2_SERVICES = %w(
+  st2actionrunner st2api st2stream
+  st2auth st2garbagecollector st2notifier
+  st2resultstracker st2rulesengine st2sensorcontainer
+).freeze
+
 describe 'stackstorm::_services' do
   before do
     global_stubs_include_recipe
@@ -9,7 +17,6 @@ describe 'stackstorm::_services' do
     let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
     let(:node) { chef_run.node }
 
-    ST2_SERVICES = %w(st2actionrunner st2api st2stream st2auth st2garbagecollector st2notifier st2resultstracker st2rulesengine st2sensorcontainer).freeze
     ST2_SERVICES.each do |service_name|
       it "should enable and start service '#{service_name}'" do
         expect(chef_run).to enable_service(service_name)
