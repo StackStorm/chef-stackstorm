f6836203057b470ed492ba98400f7c011b650acd
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-08 22:52:19 +0100

    ChefSpec: test 'stackstorm::user' for Ubuntu 14.04 and Centos 7

--- spec/recipes/user_spec.rb
+++ spec/recipes/user_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
 describe 'stackstorm::user' do
-  context 'with default node attributes' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:chef_run_ubuntu_1404) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
+  let(:chef_run_centos_7) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
 
+  context 'using ubuntu 14.04 with default node attributes' do
     it 'should create user "stanley"' do
-      expect(chef_run).to create_user('stanley').with(
+      expect(chef_run_ubuntu_1404).to create_user('stanley').with(
         home: '/home/stanley',
         manage_home: true
       )
     end
 
     it 'should create directory "creating /home/stanley/.ssh directory for stanley"' do
-      expect(chef_run).to create_directory('creating /home/stanley/.ssh directory for stanley').with(
+      expect(chef_run_ubuntu_1404).to create_directory('creating /home/stanley/.ssh directory for stanley').with(
         path: '/home/stanley/.ssh',
         owner: 'stanley',
         group: 'stanley',
@@ -21,7 +22,7 @@ describe 'stackstorm::user' do
     end
 
     it 'should run execute "generating ssh key-pair for stanley"' do
-      expect(chef_run).to run_execute('generating ssh key-pair for stanley').with(
+      expect(chef_run_ubuntu_1404).to run_execute('generating ssh key-pair for stanley').with(
         command: "ssh-keygen -f /home/stanley/.ssh/id_rsa -b 2048 -P ''",
         user: 'stanley',
         group: 'stanley'
@@ -31,7 +32,7 @@ describe 'stackstorm::user' do
     it 'should not run execute "generating ssh key-pair for stanley" if ssh key already exists' do
       allow(File).to receive(:exist?).and_call_original
       allow(File).to receive(:exist?).with('/home/stanley/.ssh/id_rsa').and_return(true)
-      expect(chef_run).to_not run_execute('generating ssh key-pair for stanley').with(
+      expect(chef_run_ubuntu_1404).to_not run_execute('generating ssh key-pair for stanley').with(
         command: "ssh-keygen -f /home/stanley/.ssh/id_rsa -b 2048 -P ''",
         user: 'stanley',
         group: 'stanley'
@@ -39,17 +40,17 @@ describe 'stackstorm::user' do
     end
 
     it 'should not create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
-      expect(chef_run).to_not create_file('creating /home/stanley/.ssh/id_rsa key stanley')
+      expect(chef_run_ubuntu_1404).to_not create_file('creating /home/stanley/.ssh/id_rsa key stanley')
     end
 
     it 'should not create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
-      expect(chef_run).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
+      expect(chef_run_ubuntu_1404).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
     end
 
     it 'should create file "creating /home/stanley/.ssh/authorized_keys for stanley"' do
       allow(IO).to receive(:read).and_call_original
       allow(IO).to receive(:read).with('/home/stanley/.ssh/id_rsa.pub').and_return('ssh-rsa fake-key')
-      expect(chef_run).to create_file('creating /home/stanley/.ssh/authorized_keys for stanley').with(
+      expect(chef_run_ubuntu_1404).to create_file('creating /home/stanley/.ssh/authorized_keys for stanley').with(
         path: '/home/stanley/.ssh/authorized_keys',
         owner: 'stanley',
         group: 'stanley',
@@ -59,34 +60,26 @@ describe 'stackstorm::user' do
     end
 
     it 'should install sudo "stanley"' do
-      expect(chef_run).to install_sudo('stanley').with(
+      expect(chef_run_ubuntu_1404).to install_sudo('stanley').with(
         user: 'stanley',
         nopasswd: true
       )
     end
   end
 
-  context "with node['stackstorm']['user']['enable_sudo'] = false" do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
-        node.normal['stackstorm']['user']['enable_sudo'] = false
-      end.converge(described_recipe)
-    end
-
+  context "using ubuntu 14.04 with node['stackstorm']['user']['enable_sudo'] = false" do
     it 'should not install sudo "stanley"' do
-      expect(chef_run).to_not install_sudo('stanley')
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['enable_sudo'] = false
+      chef_run_ubuntu_1404.converge(described_recipe)
+      expect(chef_run_ubuntu_1404).to_not install_sudo('stanley')
     end
   end
 
-  context "with node['stackstorm']['user']['ssh_key'] not nil" do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
-        node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
-      end.converge(described_recipe)
-    end
-
+  context "using ubuntu 14.04 with node['stackstorm']['user']['ssh_key'] not nil" do
     it 'should create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
-      expect(chef_run).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_ubuntu_1404.converge(described_recipe)
+      expect(chef_run_ubuntu_1404).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
         path: '/home/stanley/.ssh/id_rsa',
         mode: 0640,
         user: 'stanley',
@@ -96,20 +89,18 @@ describe 'stackstorm::user' do
     end
 
     it 'should not create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
-      expect(chef_run).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_ubuntu_1404.converge(described_recipe)
+      expect(chef_run_ubuntu_1404).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
     end
   end
 
-  context "with node['stackstorm']['user']['ssh_key'] and ['stackstorm']['user']['ssh_pub'] not nil" do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
-        node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
-        node.normal['stackstorm']['user']['ssh_pub'] = 'fake ssh public key'
-      end.converge(described_recipe)
-    end
-
+  context "using ubuntu 14.04 with node['stackstorm']['user']['ssh_key'] and ['stackstorm']['user']['ssh_pub'] not nil" do
     it 'should create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
-      expect(chef_run).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_pub'] = 'fake ssh public key'
+      chef_run_ubuntu_1404.converge(described_recipe)
+      expect(chef_run_ubuntu_1404).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
         path: '/home/stanley/.ssh/id_rsa',
         mode: 0640,
         user: 'stanley',
@@ -119,7 +110,128 @@ describe 'stackstorm::user' do
     end
 
     it 'should create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
-      expect(chef_run).to create_file('creating /home/stanley/.ssh/id_rsa public key for stanley').with(
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_ubuntu_1404.node.normal['stackstorm']['user']['ssh_pub'] = 'fake ssh public key'
+      chef_run_ubuntu_1404.converge(described_recipe)
+      expect(chef_run_ubuntu_1404).to create_file('creating /home/stanley/.ssh/id_rsa public key for stanley').with(
+        path: '/home/stanley/.ssh/id_rsa.pub',
+        user: 'stanley',
+        group: 'stanley',
+        content: 'fake ssh public key'
+      )
+    end
+  end
+
+  context 'using centos 7 with default node attributes' do
+    it 'should create user "stanley"' do
+      expect(chef_run_centos_7).to create_user('stanley').with(
+        home: '/home/stanley',
+        manage_home: true
+      )
+    end
+
+    it 'should create directory "creating /home/stanley/.ssh directory for stanley"' do
+      expect(chef_run_centos_7).to create_directory('creating /home/stanley/.ssh directory for stanley').with(
+        path: '/home/stanley/.ssh',
+        owner: 'stanley',
+        group: 'stanley',
+        mode: 0700
+      )
+    end
+
+    it 'should run execute "generating ssh key-pair for stanley"' do
+      expect(chef_run_centos_7).to run_execute('generating ssh key-pair for stanley').with(
+        command: "ssh-keygen -f /home/stanley/.ssh/id_rsa -b 2048 -P ''",
+        user: 'stanley',
+        group: 'stanley'
+      )
+    end
+
+    it 'should not run execute "generating ssh key-pair for stanley" if ssh key already exists' do
+      allow(File).to receive(:exist?).and_call_original
+      allow(File).to receive(:exist?).with('/home/stanley/.ssh/id_rsa').and_return(true)
+      expect(chef_run_centos_7).to_not run_execute('generating ssh key-pair for stanley').with(
+        command: "ssh-keygen -f /home/stanley/.ssh/id_rsa -b 2048 -P ''",
+        user: 'stanley',
+        group: 'stanley'
+      )
+    end
+
+    it 'should not create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
+      expect(chef_run_centos_7).to_not create_file('creating /home/stanley/.ssh/id_rsa key stanley')
+    end
+
+    it 'should not create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
+      expect(chef_run_centos_7).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
+    end
+
+    it 'should create file "creating /home/stanley/.ssh/authorized_keys for stanley"' do
+      allow(IO).to receive(:read).and_call_original
+      allow(IO).to receive(:read).with('/home/stanley/.ssh/id_rsa.pub').and_return('ssh-rsa fake-key')
+      expect(chef_run_centos_7).to create_file('creating /home/stanley/.ssh/authorized_keys for stanley').with(
+        path: '/home/stanley/.ssh/authorized_keys',
+        owner: 'stanley',
+        group: 'stanley',
+        mode: 0644,
+        content: "# Generated by Chef. Don't edit!\nssh-rsa fake-key"
+      )
+    end
+
+    it 'should install sudo "stanley"' do
+      expect(chef_run_centos_7).to install_sudo('stanley').with(
+        user: 'stanley',
+        nopasswd: true
+      )
+    end
+  end
+
+  context "using centos 7 with node['stackstorm']['user']['enable_sudo'] = false" do
+    it 'should not install sudo "stanley"' do
+      chef_run_centos_7.node.normal['stackstorm']['user']['enable_sudo'] = false
+      chef_run_centos_7.converge(described_recipe)
+      expect(chef_run_centos_7).to_not install_sudo('stanley')
+    end
+  end
+
+  context "using centos 7 with node['stackstorm']['user']['ssh_key'] not nil" do
+    it 'should create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_centos_7.converge(described_recipe)
+      expect(chef_run_centos_7).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
+        path: '/home/stanley/.ssh/id_rsa',
+        mode: 0640,
+        user: 'stanley',
+        group: 'stanley',
+        content: 'fake ssh key'
+      )
+    end
+
+    it 'should not create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_centos_7.converge(described_recipe)
+      expect(chef_run_centos_7).to_not create_file('creating /home/stanley/.ssh/id_rsa public key for stanley')
+    end
+  end
+
+  context "using centos 7 with node['stackstorm']['user']['ssh_key'] and ['stackstorm']['user']['ssh_pub'] not nil" do
+    it 'should create file "creating /home/stanley/.ssh/id_rsa key stanley"' do
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_pub'] = 'fake ssh public key'
+      chef_run_centos_7.converge(described_recipe)
+      expect(chef_run_centos_7).to create_file('creating /home/stanley/.ssh/id_rsa key stanley').with(
+        path: '/home/stanley/.ssh/id_rsa',
+        mode: 0640,
+        user: 'stanley',
+        group: 'stanley',
+        content: 'fake ssh key'
+      )
+    end
+
+    it 'should create file "creating /home/stanley/.ssh/id_rsa public key for stanley"' do
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_key'] = 'fake ssh key'
+      chef_run_centos_7.node.normal['stackstorm']['user']['ssh_pub'] = 'fake ssh public key'
+      chef_run_centos_7.converge(described_recipe)
+      expect(chef_run_centos_7).to create_file('creating /home/stanley/.ssh/id_rsa public key for stanley').with(
         path: '/home/stanley/.ssh/id_rsa.pub',
         user: 'stanley',
         group: 'stanley',
