49a8b79163a05d06be3e9b80ce8bbeda4c3e93e7
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-08 22:29:45 +0100

    ChefSpec: test 'stackstorm::default' for Ubuntu 14.04 and Centos 7

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -5,12 +5,13 @@ describe 'stackstorm::default' do
     global_stubs_include_recipe
   end
 
-  context 'with default node attributes' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:chef_run_ubuntu_1404) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
+  let(:chef_run_centos_7) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
 
+  context 'using ubuntu 14.04 with default node attributes' do
     it 'should include recipe stackstorm::_repository' do
       expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_repository')
-      chef_run
+      chef_run_ubuntu_1404
     end
 
     it 'should install pacakge "st2"' do
@@ -19,16 +20,41 @@ describe 'stackstorm::default' do
 
     it 'should include recipe stackstorm::config' do
       expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::config')
-      chef_run
+      chef_run_ubuntu_1404
     end
 
     it 'should include recipe stackstorm::_services' do
       expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_services')
-      chef_run
+      chef_run_ubuntu_1404
     end
 
     it 'should generate htpasswd file with auth credentials' do
-      expect(chef_run).to add_htpasswd(':add credentials to htpasswd file').with(
+      expect(chef_run_ubuntu_1404).to add_htpasswd(':add credentials to htpasswd file').with(
+        file: '/etc/st2/htpasswd',
+        user: 'st2admin',
+        password: 'Ch@ngeMe'
+      )
+    end
+  end
+
+  context 'using centos 7 with default node attributes' do
+    it 'should include recipe stackstorm::_packages' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_packages')
+      chef_run_centos_7
+    end
+
+    it 'should include recipe stackstorm::config' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::config')
+      chef_run_centos_7
+    end
+
+    it 'should include recipe stackstorm::_services' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_services')
+      chef_run_centos_7
+    end
+
+    it 'should generate htpasswd file with auth credentials' do
+      expect(chef_run_centos_7).to add_htpasswd(':add credentials to htpasswd file').with(
         file: '/etc/st2/htpasswd',
         user: 'st2admin',
         password: 'Ch@ngeMe'
