8ca3aa66eec3897d750ffc629ccc4391a9056c80
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-08 22:28:09 +0100

    ChefSpec: test 'stackstorm::_services' for Ubuntu 14.04 and Centos 7

--- spec/recipes/_services_spec.rb
+++ spec/recipes/_services_spec.rb
@@ -13,14 +13,23 @@ describe 'stackstorm::_services' do
     global_stubs_include_recipe
   end
 
-  context 'with default node attributes' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
-    let(:node) { chef_run.node }
+  let(:chef_run_ubuntu_1404) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
+  let(:chef_run_centos_7) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
 
+  context 'using ubuntu 14.04 with default node attributes' do
     ST2_SERVICES.each do |service_name|
       it "should enable and start service '#{service_name}'" do
-        expect(chef_run).to enable_service(service_name)
-        expect(chef_run).to start_service(service_name)
+        expect(chef_run_ubuntu_1404).to enable_service(service_name)
+        expect(chef_run_ubuntu_1404).to start_service(service_name)
+      end
+    end
+  end
+
+  context 'using centos 7 with default node attributes' do
+    ST2_SERVICES.each do |service_name|
+      it "should enable and start service '#{service_name}'" do
+        expect(chef_run_centos_7).to enable_service(service_name)
+        expect(chef_run_centos_7).to start_service(service_name)
       end
     end
   end
