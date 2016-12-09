39d32da52921488ef69c3cdc54eaa2e940fc31d8
armab <dev@arma.biz>
2016-12-09 14:27:11 +0200

    Web recipe should include PackageCloud repo install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
 default['stackstorm']['username'] = 'st2admin'
 default['stackstorm']['password'] = 'Ch@ngeMe'
-default['stackstorm']['install_repo']['packages'] = %w(st2)
--- spec/recipes/web_spec.rb
+++ spec/recipes/web_spec.rb
@@ -8,6 +8,11 @@ describe 'stackstorm::web' do
   context 'with default node attributes' do
     let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
+    it 'should include recipe stackstorm::_repository' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_repository')
+      chef_run
+    end
+
     it 'should install pacakge "st2web"' do
       expect(chef_run).to install_package('st2web')
     end
