067f7462e7918096a6df5177ac5ca93d32e491ea
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-09 15:25:50 +0100

    ChefSpec: stackstorm::default. DRY, iterate over the platforms

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -5,60 +5,39 @@ describe 'stackstorm::default' do
     global_stubs_include_recipe
   end
 
-  let(:chef_run_ubuntu_1404) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
-  let(:chef_run_centos_7) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
+  platforms = {
+    'ubuntu' => ['14.04'],
+    'centos' => ['7.0']
+  }
 
-  context 'using ubuntu 14.04 with default node attributes' do
-    it 'should include recipe stackstorm::_repository' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_repository')
-      chef_run_ubuntu_1404
-    end
+  platforms.each do |platform, versions|
+    versions.each do |version|
+      context "Using #{platform} #{version} with default node attributes" do
+        let(:chef_run) { ChefSpec::SoloRunner.new(platform: platform, version: version).converge(described_recipe) }
 
-    it 'should install pacakge "st2"' do
-      expect(chef_run).to install_package('st2')
-    end
+        it 'should include recipe stackstorm::_repository' do
+          expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_repository')
+          chef_run
+        end
 
-    it 'should include recipe stackstorm::config' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::config')
-      chef_run_ubuntu_1404
-    end
+        it 'should include recipe stackstorm::config' do
+          expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::config')
+          chef_run
+        end
 
-    it 'should include recipe stackstorm::_services' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_services')
-      chef_run_ubuntu_1404
-    end
+        it 'should include recipe stackstorm::_services' do
+          expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_services')
+          chef_run
+        end
 
-    it 'should generate htpasswd file with auth credentials' do
-      expect(chef_run_ubuntu_1404).to add_htpasswd(':add credentials to htpasswd file').with(
-        file: '/etc/st2/htpasswd',
-        user: 'st2admin',
-        password: 'Ch@ngeMe'
-      )
-    end
-  end
-
-  context 'using centos 7 with default node attributes' do
-    it 'should include recipe stackstorm::_packages' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_packages')
-      chef_run_centos_7
-    end
-
-    it 'should include recipe stackstorm::config' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::config')
-      chef_run_centos_7
-    end
-
-    it 'should include recipe stackstorm::_services' do
-      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('stackstorm::_services')
-      chef_run_centos_7
-    end
-
-    it 'should generate htpasswd file with auth credentials' do
-      expect(chef_run_centos_7).to add_htpasswd(':add credentials to htpasswd file').with(
-        file: '/etc/st2/htpasswd',
-        user: 'st2admin',
-        password: 'Ch@ngeMe'
-      )
+        it 'should generate htpasswd file with auth credentials' do
+          expect(chef_run).to add_htpasswd(':add credentials to htpasswd file').with(
+            file: '/etc/st2/htpasswd',
+            user: 'st2admin',
+            password: 'Ch@ngeMe'
+          )
+        end
+      end
     end
   end
 end
