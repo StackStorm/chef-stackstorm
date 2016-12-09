426e28d98780d44fb0bebc9ea0b991fce3254205
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-09 16:10:43 +0100

    ChefSpec: stackstorm::_repository. DRY, iterate over the platforms

--- spec/recipes/_repository_spec.rb
+++ spec/recipes/_repository_spec.rb
@@ -8,22 +8,35 @@ describe 'stackstorm::_repository' do
   let(:chef_run_ubuntu_1404) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
   let(:chef_run_centos_7) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
 
-  context 'using ubuntu 14.04 with default node attributes' do
-    let(:execute) { chef_run_ubuntu_1404.execute('Ubuntu Precise and Trusty: APT hash sum mismatch workaround') }
+  platforms = {
+    'ubuntu' => ['14.04'],
+    'centos' => ['7.0']
+  }
 
-    it 'should include recipe apt' do
-      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('apt')
-      chef_run_ubuntu_1404
+  platforms.each do |platform, versions|
+    versions.each do |version|
+      context "Using #{platform} #{version} with default node attributes" do
+        let(:chef_run) { ChefSpec::SoloRunner.new(platform: platform, version: version).converge(described_recipe) }
+
+        it 'should include recipe apt' do
+          allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('apt')
+          chef_run
+        end
+
+        it 'should not install pacakge "apt"' do
+          expect(chef_run).to_not install_package('apt').with(version: '1.2.10')
+        end
+      end
     end
+  end
+
+  context 'Using ubuntu 14.04 with default node attributes' do
+    let(:execute) { chef_run_ubuntu_1404.execute('Ubuntu Precise and Trusty: APT hash sum mismatch workaround') }
 
     it 'should create packagecloud_repo "StackStorm/stable"' do
       expect(chef_run_ubuntu_1404).to create_packagecloud_repo('StackStorm/stable').with(
         type: 'deb'
       )
-    end
-
-    it 'should not install pacakge "apt"' do
-      expect(chef_run_ubuntu_1404).to_not install_package('apt').with(version: '1.2.10')
     end
 
     it 'should run a execute "Ubuntu Precise and Trusty: APT hash sum mismatch workaround"' do
@@ -35,22 +48,13 @@ describe 'stackstorm::_repository' do
     end
   end
 
-  context 'using centos 7 with default node attributes' do
+  context 'Using centos 7.0 with default node attributes' do
     let(:execute) { chef_run_centos_7.execute('Ubuntu Precise and Trusty: APT hash sum mismatch workaround') }
-
-    it 'should include recipe apt' do
-      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('apt')
-      chef_run_centos_7
-    end
 
     it 'should create packagecloud_repo "StackStorm/stable"' do
       expect(chef_run_centos_7).to create_packagecloud_repo('StackStorm/stable').with(
         type: 'rpm'
       )
-    end
-
-    it 'should not install pacakge "apt"' do
-      expect(chef_run_centos_7).to_not install_package('apt').with(version: '1.2.10')
     end
 
     it 'should not run a execute "Ubuntu Precise and Trusty: APT hash sum mismatch workaround"' do
