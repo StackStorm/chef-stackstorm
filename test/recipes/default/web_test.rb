4cef475f23d53b05b27f93825a48ea7729ea9731
armab <dev@arma.biz>
2016-12-09 16:49:12 +0200

    Remove missed debugging from the web recipe

--- test/recipes/default/web_test.rb
+++ test/recipes/default/web_test.rb
@@ -13,8 +13,6 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-puts package('nginx').version
-
 describe service('nginx') do
   it { should be_installed }
   it { should be_enabled }
