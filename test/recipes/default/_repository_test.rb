72677c19f132fd0e0d908e32b8e75387e92817e2
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2017-03-07 21:21:47 +0100

    Rename inspec test file to match recipe name

--- test/recipes/default/_repository_test.rb
+++ test/recipes/default/_repository_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe stackstorm::_packages
+# Inspec test for recipe stackstorm::_repository
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
