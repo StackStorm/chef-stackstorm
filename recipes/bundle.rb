02ab2bfc5944758c39a96767850d5dbda50e22a0
armab <dev@arma.biz>
2017-03-31 19:32:27 +0300

    Just pin to 'rabbitmq' '4.10.0'

--- recipes/bundle.rb
+++ recipes/bundle.rb
@@ -42,11 +42,6 @@ node.default['stackstorm']['user']['ssh_pub'] = pubkey
 node.default['stackstorm']['user']['ssh_key'] = prikey
 node.default['stackstorm']['user']['authorized_keys'] = [pubkey]
 
-node.override['rabbitmq']['version'] = '3.6.9'
-# workaround for 'derived attributes' to force override rabbitmq version
-# See: https://tickets.opscode.com/browse/CHEF-4234
-node.from_file(run_context.resolve_attribute('rabbitmq', 'default'))
-
 include_recipe 'mongodb3::default'
 include_recipe 'rabbitmq::default'
 include_recipe 'stackstorm::default'
--- recipes/mistral.rb
+++ recipes/mistral.rb
@@ -5,11 +5,6 @@
 # Copyright (C) 2015 StackStorm (info@stackstorm.com)
 #
 
-node.override['rabbitmq']['version'] = '3.6.9'
-# workaround for 'derived attributes' to force override rabbitmq version
-# See: https://tickets.opscode.com/browse/CHEF-4234
-node.from_file(run_context.resolve_attribute('rabbitmq', 'default'))
-
 node.override['openstack-mistral']['etc_dir'] = '/etc/mistral'
 node.override['openstack-mistral']['db_initialize']['enabled'] = true
 node.override['openstack-mistral']['db_initialize']['password'] = 'ilikerandompasswords'
