1eba576c8bcfb71f5d6746a4e7149fc32f05320c
Daniel Jolly <djolly@fuze.com>
2019-12-10 11:03:31 -0500

    Fixed spec and recipes to handle new st2auth config

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@stackstorm.com'
 license 'Apache-2.0'
 description 'Installs/Configures stackstorm'
 long_description 'Installs/Configures stackstorm'
-version '0.7.0'
+version '0.7.1'
 
 supports 'ubuntu', '= 14.04'
 supports 'redhat', '~> 7.5'
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,9 +11,11 @@ include_recipe 'stackstorm::config'
 include_recipe 'stackstorm::_services'
 
 # Generate username & password for htpasswd flat-file authentication
-htpasswd ':add credentials to htpasswd file' do
-  file node['stackstorm']['config']['auth_standalone_file']
-  user node['stackstorm']['username']
-  password node['stackstorm']['password']
-  action :add
+if node['stackstorm']['config']['auth_backend'] == 'flat_file'
+  htpasswd ':add credentials to htpasswd file' do
+    file node['stackstorm']['config']['auth_backend_kwargs']['file_path']
+    user node['stackstorm']['username']
+    password node['stackstorm']['password']
+    action :add
+  end
 end
